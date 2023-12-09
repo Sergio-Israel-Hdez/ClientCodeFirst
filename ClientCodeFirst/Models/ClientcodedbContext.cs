1b76c3d8e8a4a2fb5d3643af9d4dfdfd3d4ac4d9
Sergio Flores <sergio.israel.hernandez@gmail.com>
2023-12-09 12:36:05 -0600

    cadena de conexion modificada

--- ClientCodeFirst/Models/ClientcodedbContext.cs
+++ ClientCodeFirst/Models/ClientcodedbContext.cs
@@ -24,8 +24,7 @@ public partial class ClientcodedbContext : DbContext
 
     */
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=mysql-service.default.svc.cluster.local;port=3311;user=root;password=root;database=clientcodedb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.32-mysql"));
+        => optionsBuilder.UseMySql("server=192.168.1.11;port=30011;user=root;password=root;database=clientcodedb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.32-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
