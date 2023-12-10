f7e12ceec124bf457c5de3b1452307099229cbf5
Sergio Flores <sergio.israel.hernandez@gmail.com>
2023-12-10 11:03:09 -0600

    se modifico la forma de conectarse  a mysql y se envia variables de entorno

--- ClientCodeFirst/Controllers/ClienteController.cs
+++ ClientCodeFirst/Controllers/ClienteController.cs
@@ -1,14 +1,29 @@
 
+using System.Data;
 using ClientCodeFirst.Models;
 using Microsoft.AspNetCore.Mvc;
-
+using MySql.Data.MySqlClient;
 namespace ClientCodeFirst.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
 public class ClienteController : ControllerBase
 {
-    
+
+    private readonly string connString;
+    private readonly IConfiguration _configuration;
+    private readonly ILogger<ClienteController> _logger;
+    public ClienteController(IConfiguration configuration, ILogger<ClienteController> logger)
+    {
+        _logger = logger;
+        var host = _configuration["DBHOST"];
+        var port = _configuration["DBPORT"];
+        var password = _configuration["MYSQL_PASSWORD"];
+        var userid = _configuration["MYSQL_USER"];
+        var usersDataBase = _configuration["MYSQL_DATABASE"];
+
+        connString = $"server={host}; userid={userid};pwd={password};port={port};database={usersDataBase}";
+    }
     [HttpGet]
     [Route("[action]")]
     public IEnumerable<Cliente> GetAll()
@@ -18,7 +33,33 @@ public class ClienteController : ControllerBase
             return _context.Clientes.ToList();
         }
     }
-    
+    [HttpGet]
+    [Route("[action]")]
+    public IEnumerable<Cliente> GetAllMySql()
+    {
+        List<Cliente> clientes = new List<Cliente>();
+        using (var conn = new MySqlConnection(connString))
+        {
+            conn.Open();
+            using (var cmd = new MySqlCommand("select * from Cliente",conn))
+            {
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Cliente cliente = new Cliente(){
+                            IdCliente = (int)reader["IdCliente"],
+                            Nombre = (string)reader["Nombre"],
+                            CorreoElectronico = (string)reader["CorreoElectronico"],
+                            Telefono = (string)reader["Telefono"]
+                        };
+                        clientes.Add(cliente);
+                    }
+                }
+            }
+        }
+        return clientes.AsEnumerable();
+    }
     [HttpPost]
     [Route("[action]")]
     public void Insert([FromBody] Cliente cliente)
@@ -29,4 +70,5 @@ public class ClienteController : ControllerBase
             _context.SaveChanges();
         }
     }
+
 }
